a41bcbf452ea0d9a238b0ed30700fae0d0b0e5bd
Marat Sayfutdinov <saymon.161@gmail.com>
2018-06-10 11:36:37 +0300

    style welcome-page(index.pug)

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,8 +1,9 @@
 module.exports = {
   white: "#efeee9",
   beige: "#eeebdf",
-  green: "#16c2aa",
+  green: "#618f54",
   "text-color": "#455a64",
+  darkgreen: "#4e8839",
   grey: "#373e42",
   "light-grey": "#dfdcd5"
 };
