f335ce95df9364c060b907ea1dd68367486182d9
Marat Sayfutdinov <saymon.161@gmail.com>
2018-06-11 13:32:21 +0300

    add hero section started about

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,8 +1,8 @@
 module.exports = {
-  white: "#efeee9",
-  beige: "#eeebdf",
-  green: "#618f54",
-  "text-color": "#455a64",
+  white: "#f4f5f0",
+  beige: "#ebefe2",
+  green: "#6c9c5a",
+  "text-color": "#566358",
   darkgreen: "#4e8839",
   grey: "#373e42",
   "light-grey": "#dfdcd5"
