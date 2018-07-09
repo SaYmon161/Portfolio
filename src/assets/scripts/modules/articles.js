f7f490e3a960712aba0142704ce46d387016ff41
Marat Sayfutdinov <saymon.161@gmail.com>
2018-07-09 08:00:54 +0300

    add blog aside fillup

--- src/assets/scripts/modules/articles.js
+++ src/assets/scripts/modules/articles.js
@@ -1,8 +1,35 @@
+import axios from "axios";
+
 const blogList = document.querySelector(".blog__list");
 const articles = document.querySelectorAll(".article");
 const links = document.querySelectorAll(".blog__link");
 const pullTag = document.querySelector(".blog__pull-tag");
 let menuOpened = false;
+let blogArticles;
+
+axios.get("http://webdev-api.loftschool.com/posts/16").then(response => {
+  blogArticles = response.data;
+  console.log(response.data);
+  for (let article of blogArticles) {
+    const asideItem = document.createElement("li");
+    const asideLink = document.createElement("a", { href: "#" });
+    asideItem.classList.add("blog__item");
+    asideLink.classList.add("blog__link");
+    asideLink.setAttribute("href", "#");
+    asideLink.setAttribute("data-name", `${article.title}`);
+    asideItem.appendChild(asideLink);
+    // console.log(asideLink);
+
+    // console.log(asideItem);
+    // asideItem.innerHTML = `
+    // <a class="blog__link" href="#" data-name=${article.title}> ${
+    //   article.title
+    // } </a>
+    // `;
+    console.log(asideItem);
+    blogList.appendChild(asideItem);
+  }
+});
 
 links[0].parentElement.classList.add("blog__item--active");
 
